62f2a8f15042b5ebb6238d629879736d17bca27b
WintersleepAI <wintersleep@protonmail.com>
2021-09-20 15:41:22 -0500

    wip on ship sheet

--- src/module/actors/ship-sheet.ts
+++ src/module/actors/ship-sheet.ts
@@ -63,9 +63,67 @@ export class ShipActorSheet extends ActorSheet<
   
     activateListeners(html: JQuery): void {
       super.activateListeners(html);
+      html.find(".item-edit").on("click", this._onItemEdit.bind(this));
+      html.find(".item-delete").on("click", this._onItemDelete.bind(this));
+      html.find(".item-toggle-broken").on("click", this._onItemBreakToggle.bind(this));
+      html.find(".item-click").on("click", this._onItemClick.bind(this));
+
     }
   
+    // Clickable title/name or icon. Invoke Item.roll()
+    _onItemClick(event: JQuery.ClickEvent): void {
+      event.preventDefault();
+      event.stopPropagation();
+      const itemId = event.currentTarget.parentElement.dataset.itemId;
+      const item = <SWNRBaseItem>(
+        this.actor.getEmbeddedDocument("Item", itemId)
+      );
+      //const wrapper = $(event.currentTarget).parents(".item");
+      //const item = this.actor.getEmbeddedDocument("Item", wrapper.data("itemId"));
+      if (!item) return;
+      item.roll();
+    }
 
+    _onItemBreakToggle(event: JQuery.ClickEvent): void {
+      event.preventDefault();
+      event.stopPropagation();
+      const wrapper = $(event.currentTarget).parents(".item");
+      const item = this.actor.getEmbeddedDocument("Item", wrapper.data("itemId"));
+      const new_break_status = !item?.data.data.broken;
+      if (item instanceof Item) item?.update({"data.broken" : new_break_status});
+
+    }
+    _onItemEdit(event: JQuery.ClickEvent): void {
+      event.preventDefault();
+      event.stopPropagation();
+      const wrapper = $(event.currentTarget).parents(".item");
+      const item = this.actor.getEmbeddedDocument("Item", wrapper.data("itemId"));
+      if (item instanceof Item) item.sheet?.render(true);
+    }
+    async _onItemDelete(event: JQuery.ClickEvent): Promise<void> {
+      event.preventDefault();
+      event.stopPropagation();
+      const li = $(event.currentTarget).parents(".item");
+      const item = this.actor.getEmbeddedDocument("Item", li.data("itemId"));
+      if (!item) return;
+      const performDelete: boolean = await new Promise((resolve) => {
+        Dialog.confirm({
+          title: game.i18n.format("swnr.deleteTitle", { name: item.name }),
+          yes: () => resolve(true),
+          no: () => resolve(false),
+          content: game.i18n.format("swnr.deleteContent", {
+            name: item.name,
+            actor: this.actor.name,
+          }),
+        });
+      });
+      if (!performDelete) return;
+      li.slideUp(200, () => {
+        requestAnimationFrame(() => {
+          this.actor.deleteEmbeddedDocuments("Item", [li.data("itemId")]);
+        });
+      });
+    }
 }
 export const sheet = ShipActorSheet;
 export const types = ["ship"];
