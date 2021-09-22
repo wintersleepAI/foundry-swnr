089d39382b0dd395cacd7eae360830af46fd131e
WintersleepAI <wintersleep@protonmail.com>
2021-09-22 08:36:13 -0500

    wip

--- src/module/items/shipWeapon.ts
+++ src/module/items/shipWeapon.ts
@@ -1,3 +1,35 @@
 import { SWNRBaseItem } from "../base-item";
-export const document = SWNRBaseItem;
+import { ValidatedDialog } from "../ValidatedDialog";
+import { SWNRCharacterActor } from "../actors/character";
+import { SWNRNPCActor } from "../actors/npc";
+
+export class SWNRShipWeapon extends SWNRBaseItem<"weapon"> {
+  popUpDialog?: Dialog;
+  async roll(): Promise<void> {
+    ui.notifications?.info("rolling");
+    if (!this.actor) {
+      const message = `Called ship weapon.roll on item without an actor.`;
+      ui.notifications?.error(message);
+      new Error(message);
+      return;
+    }
+    if (this.actor.type=="ship"){
+      let defaultGunner = this.actor.data.data.roles.gunnery;
+      let crewArray: Array<SWNRCharacterActor| SWNRNPCActor> = [];
+      if (this.actor.data.data.crewMembers) {
+        for (var i in this.actor.data.data.crewMembers ){
+          let cId = this.actor.data.data.crewMembers[i];
+          let crewMember=game.actors?.get(cId);
+          if (crewMember && (crewMember.type=="character" || crewMember.type=="npc")){
+            crewArray.push(crewMember);
+          }  
+        }
+      }
+      
+      
+    }
+  }
+}
+
+export const document = SWNRShipWeapon;
 export const name = "shipWeapon";
