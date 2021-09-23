ddbdefc18560acac3308970de23b2679585649c5
WintersleepAI <wintersleep@protonmail.com>
2021-09-23 14:23:52 -0500

    ship uses pilot int/dex mod

--- src/module/combat.ts
+++ src/module/combat.ts
@@ -7,6 +7,16 @@ export class SWNRCombatant extends Combatant {
       if (actor.type == "character"  && actor.data.data.tweak?.advInit) {
         return `{${game.system.data.initiative},${game.system.data.initiative}}kh`
       }
+      if (actor.type == "ship"){
+        if (actor.data.data.roles.bridge!= ""){
+          let pilot = game.actors?.get(actor.data.data.roles.bridge);
+          if (pilot && pilot.type=="character"){
+            let mod = (pilot.data.data.stats.int.mod>=pilot.data.data.stats.dex.mod) ? pilot.data.data.stats.int.mod : pilot.data.data.stats.dex.mod;
+            return `1d8+${mod}`;
+          }
+        }
+        return "1d8";
+      }
       return init;
     } else {
       ui.notifications?.info("Error getting init roll or actor. Falling back on 1d8");
