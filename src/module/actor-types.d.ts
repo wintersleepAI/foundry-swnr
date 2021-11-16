1493d8360bd153c0886b969cf1b088d65243ac09
wintersleepAI <wintersleep@protonmail.com>
2021-11-16 17:22:47 -0600

    ships in prog

--- src/module/actor-types.d.ts
+++ src/module/actor-types.d.ts
@@ -334,6 +334,7 @@ declare interface SWNRShipData extends SWNRVehicleTemplateBase {
   };
   cargoCarried: SWNRResource[];
   commandPoints: number;
+  npcCommandPoints: number;
   crewSkillBonus: number;
 }
 
