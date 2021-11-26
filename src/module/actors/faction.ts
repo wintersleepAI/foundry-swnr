e54067fefa25876cb2fd40fed690dfacffe2835a
wintersleepAI <wintersleep@protonmail.com>
2021-11-25 23:30:07 -0600

    more wip on faction page

--- src/module/actors/faction.ts
+++ src/module/actors/faction.ts
@@ -8,6 +8,18 @@ export class SWNRFactionActor extends SWNRBaseActor<"faction"> {
     // data.itemTypes = <SWNRCharacterData["itemTypes"]>this.itemTypes;
     return data;
   }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  async _preCreate(actorDataConstructorData, options, user): Promise<void> {
+    await super._preCreate(actorDataConstructorData, options, user);
+    if (
+      actorDataConstructorData.type &&
+      this.data._source.img == "icons/svg/mystery-man.svg"
+    ) {
+      const img = "systems/swnr/assets/icons/faction.png";
+      this.data._source.img = img;
+    }
+  }
 }
 
 export const document = SWNRFactionActor;
