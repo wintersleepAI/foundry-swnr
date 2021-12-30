599ff04062c545a919d6a31cb588725985659270
wintersleepAI <wintersleep@protonmail.com>
2021-12-29 22:44:30 -0600

    icons from hawkin

--- src/module/actors/faction-sheet.ts
+++ src/module/actors/faction-sheet.ts
@@ -21,6 +21,49 @@ export class FactionActorSheet extends BaseActorSheet<FactionActorSheetData> {
     super._injectHTML(html);
   }
 
+  async _onAssetCreate(event: JQuery.ClickEvent): Promise<void> {
+    event.preventDefault();
+    event.stopPropagation();
+    const itemType = $(event.currentTarget).data("itemType");
+    const givenName = $(event.currentTarget).data("itemName");
+    const itemName = givenName ? `New ${givenName}` : "New Item";
+    const imgPath = getDefaultImage(itemType);
+    if (itemType) {
+      this.actor.createEmbeddedDocuments(
+        "Item",
+        [
+          {
+            name: itemName,
+            type: itemType,
+            img: imgPath,
+          },
+        ],
+        {}
+      );
+    }
+  }
+
+  getDefaultImage(itemType: string): string | null {
+    const icon_path = "systems/swnr/assets/icons/game-icons.net/item-icons";
+    const imgMap = {
+      shipWeapon: "sinusoidal-beam.svg",
+      shipDefense: "bubble-field.svg",
+      shipFitting: "power-generator.svg",
+      cyberware: "cyber-eye.svg",
+      focus: "reticule.svg",
+      armor: "armor-white.svg",
+      weapon: "weapon-white.svg",
+      power: "psychic-waves-white.svg",
+      skill: "book-white.svg",
+    };
+    if (itemType in imgMap) {
+      return `${icon_path}/${imgMap[itemType]}`;
+    } else {
+      return "icons/svg/item-bag.svg";
+    }
+  }
+
+
   async getData(
     options?: Application.RenderOptions
   ): Promise<FactionActorSheetData> {
