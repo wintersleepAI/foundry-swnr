fe6facb336e467dc6c93f47d653c384a6e7a8895
wintersleepAI <wintersleep@protonmail.com>
2022-12-08 12:47:52 -0600

    power chat roll

--- src/module/items/power.ts
+++ src/module/items/power.ts
@@ -1,3 +1,37 @@
 import { SWNRBaseItem } from "./../base-item";
-export const document = SWNRBaseItem;
+
+export class SWNRPower extends SWNRBaseItem<"power"> {
+  popUpDialog?: Dialog;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async roll(_shiftKey = false): Promise<void> {
+    if (!this.actor) {
+      const message = `Called power.roll on item without an actor.`;
+      ui.notifications?.error(message);
+      new Error(message);
+      return;
+    }
+    const powerRoll = new Roll(this.data.data.roll ? this.data.data.roll : "0");
+    await powerRoll.roll({ async: true });
+    const dialogData = {
+      actor: this.actor.data,
+      power: this,
+      powerRoll: await powerRoll.render(),
+    };
+    const rollMode = game.settings.get("core", "rollMode");
+
+    const template = "systems/swnr/templates/chat/power-roll.html";
+    const chatContent = await renderTemplate(template, dialogData);
+    const chatData = {
+      speaker: ChatMessage.getSpeaker({ actor: this.actor ?? undefined }),
+      content: chatContent,
+      roll: JSON.stringify(powerRoll),
+      type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+    };
+    getDocumentClass("ChatMessage").applyRollMode(chatData, rollMode);
+    getDocumentClass("ChatMessage").create(chatData);
+  }
+}
+
+export const document = SWNRPower;
 export const name = "power";
