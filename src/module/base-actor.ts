75bfb01c7119be4e837249b1545de85803f89721
wintersleepAI <wintersleep@protonmail.com>
2022-12-17 22:48:29 -0600

    Adding a base rollSave to appease typescript.

--- src/module/base-actor.ts
+++ src/module/base-actor.ts
@@ -3,4 +3,11 @@ export class SWNRBaseActor<
 > extends Actor {
   //@ts-expect-error Subtype override
   data: Actor["data"] & { _source: { type: Type }; type: Type };
+
+  async rollSave(save: string): Promise<void> {
+    const roll = new Roll("1d20");
+    await roll.roll({ async: true });
+    const flavor = `Rolling Save`;
+    roll.toMessage({ flavor, speaker: { actor: this.id } });
+  }
 }
